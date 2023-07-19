a098e5661424801120136a37a23c062fe15c9093
feerrrum <139925598+feerrrum@users.noreply.github.com>
2023-07-19 19:29:57 +0500

    Create 25_2.1.py

---
+++ 25_2/25_2.1.py
@@ -0,0 +1,25 @@
+def div(x):
+    ans = []
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            ans += [i] + [x // i]
+    return ans
+
+def prime(x):
+    if x < 2:
+        return False
+    if x == 2:
+        return True
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+for x in range(1234490, 1234520):
+    d = div(x)
+    f = 1
+    for i in d:
+        if not prime(i):
+            f = 0
+    if f == 1 and len(d) > 0:
+        print(x, max(d))
