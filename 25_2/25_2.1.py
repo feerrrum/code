2b3d37bf54587e77e3ee01ca52aa07e9f9089b65
feerrrum <139925598+feerrrum@users.noreply.github.com>
2023-07-19 19:56:04 +0500

    Update 25_2.1.py

--- 25_2/25_2.1.py
+++ 25_2/25_2.1.py
@@ -1,8 +1,9 @@
 def div(x):
-    ans = []
+    ans = set()
     for i in range(2, int(x**0.5) + 1):
         if x % i == 0:
-            ans += [i] + [x // i]
+            ans.add(i)
+            ans.add(x // i)
     return ans
 
 def prime(x):
